5ee1cd72f426f73f2eb004d1d096090fda182d15
neelpatel1026 <neelpatel8422@gmail.com>
2025-12-15 18:13:58 +0530

    introduce to useState hook

---
+++ counter-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter]  = useState(0)
+
+  //let counter = 15
+
+  const addValue = () => {
+     setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
